d65caa0c43702569757f61f3684491119e10ad22
wz156483? <john@john-PC>
2016-05-26 17:25:53 +0800

    my frist use git

---
+++ src/cn/com/test/demo.java
@@ -0,0 +1,13 @@
+package cn.com.test;
+
+import org.com.xxd.BankGet;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		BankGet bank=new BankGet();
+		bank.show();
+		
+	}
+}
